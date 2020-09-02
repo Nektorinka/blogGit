55be99c70cf84bc515a57b2eae964d509fb16865
Nektorinka <62047881+Nektorinka@users.noreply.github.com>
2020-09-02 21:49:54 +0300

    upd

---
+++ build/precache-manifest.8151700d8cf16ab17247c65d21d9b397.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "892a67e322ef4c718ce20eb7c5f00e0f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "389ce357212cfdce44e2",
+    "url": "/static/css/2.04ff9426.chunk.css"
+  },
+  {
+    "revision": "5def711353d3a3e9979a",
+    "url": "/static/css/main.5a4f862e.chunk.css"
+  },
+  {
+    "revision": "389ce357212cfdce44e2",
+    "url": "/static/js/2.4359e4e1.chunk.js"
+  },
+  {
+    "revision": "2069192f75c6f3fb6247231eea52f248",
+    "url": "/static/js/2.4359e4e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5def711353d3a3e9979a",
+    "url": "/static/js/main.a569a33c.chunk.js"
+  },
+  {
+    "revision": "35c6bc6620fd0e11f636",
+    "url": "/static/js/runtime-main.0ed6e50a.js"
+  },
+  {
+    "revision": "411e58c6ff90c5fe588dcda00b91ecf2",
+    "url": "/static/media/no-label.411e58c6.svg"
+  }
+]);
